aeb74af3a01ee63311fe9d06cd2a1aa7efb6cd3e
Son Juwan <wndhks171@naver.com>
2026-01-03 01:18:31 +0900

    init/#1: core/공통 response 형식 및 dummy파일

---
+++ app/src/main/java/com/Kiero/core/network/model/DummyBaseResponse.kt
@@ -0,0 +1,18 @@
+package com.Kiero.core.network.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DummyBaseResponse<T>(
+    @SerialName("data")
+    val data: T,
+    @SerialName("page")
+    val page: Int? = null,
+    @SerialName("per_page")
+    val perPage: Int? = null,
+    @SerialName("total")
+    val total: Int? = null,
+    @SerialName("total_pages")
+    val totalPages: Int? = null
+)
