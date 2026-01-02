f4af73a428afa4fb06b3e5a60204e53812e8b6dc
Son Juwan <wndhks171@naver.com>
2026-01-03 01:19:07 +0900

    init/#1: data/mapper 세팅

---
+++ app/src/main/java/com/Kiero/data/auth/mapper/DummyMapper.kt
@@ -0,0 +1,28 @@
+package com.Kiero.data.auth.mapper
+
+import com.Kiero.data.auth.remote.dto.response.DummyResponseDto
+import com.Kiero.domain.auth.model.DummyEntity
+import javax.inject.Inject
+
+class DummyMapper @Inject constructor() {
+    fun mapDtoToEntity(dto : DummyResponseDto) : DummyEntity {
+        return DummyEntity(
+            profile = dto.avatar,
+            firstName = dto.firstName,
+            id = dto.id,
+            lastName = dto.lastName,
+        )
+    }
+
+    fun mapEntityToDto(entity : DummyEntity) : DummyResponseDto {
+        return DummyResponseDto(
+            avatar = entity.profile,
+            firstName = entity.firstName,
+            id = entity.id,
+            lastName = entity.lastName,
+            email = ""
+        )
+    }
+}
+
+
