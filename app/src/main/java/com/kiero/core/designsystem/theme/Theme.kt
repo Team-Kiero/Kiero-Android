657ba89fab12814fb27d5640f529254336e2f917
seungjae708 <choesj365@gmail.com>
2026-01-03 22:55:18 +0900

    feature/#3: 디자인 시스템 테마 정의

--- app/src/main/java/com/kiero/core/designsystem/theme/Theme.kt
+++ app/src/main/java/com/kiero/core/designsystem/theme/Theme.kt
@@ -1,57 +1,63 @@
 package com.kiero.core.designsystem.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.staticCompositionLocalOf
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+val localKieroColors = staticCompositionLocalOf { defaultKieroColors }
+
+val localKieroTypography = staticCompositionLocalOf { defaultKieroTypography }
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Black,
+    background = White,
+    surface = White,
+    onPrimary = White,
+    onBackground = Black,
+    onSurface = Black
 )
+
+object KieroTheme {
+    val colors: KieroColors
+        @Composable
+        @ReadOnlyComposable
+        get() = localKieroColors.current
+
+    val typography: KieroTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = localKieroTypography.current
+}
+
+@Composable
+fun ProvideKieroColorsAndTypography(
+    colors: KieroColors,
+    typography: KieroTypography,
+    content: @Composable () -> Unit
+) {
+    CompositionLocalProvider(
+        localKieroColors provides colors,
+        localKieroTypography provides typography,
+        content = content
+    )
+}
 
 @Composable
 fun KieroTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    ProvideKieroColorsAndTypography(
+        colors = defaultKieroColors,
+        typography = defaultKieroTypography
+    ) {
+        MaterialTheme(
+            colorScheme = LightColorScheme,
+            content = content
+        )
     }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
 }
