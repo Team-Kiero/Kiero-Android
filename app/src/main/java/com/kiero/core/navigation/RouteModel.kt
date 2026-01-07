1c756a92a6cb420337c424f9869bdc6f77914213
Son Juwan <wndhks171@naver.com>
2026-01-07 17:17:57 +0900

    setting/#4: route model 수정

--- app/src/main/java/com/kiero/core/navigation/RouteModel.kt
+++ app/src/main/java/com/kiero/core/navigation/RouteModel.kt
@@ -12,39 +12,5 @@ data object ParentGraph : Route
 @Serializable
 data object KidGraph : Route
 
-@Serializable
-sealed class Auth : Route {
-    @Serializable
-    data object Login : Auth()
-}
 
-@Serializable
-sealed class ParentTab : Route {
-    @Serializable
-    data object Schedule : ParentTab()
 
-    @Serializable
-    data object Alarm : ParentTab()
-}
-
-@Serializable
-sealed class ParentRoute : Route {
-    // TODO : PARENT 화면 내 세부화면 Route 설정
-}
-
-@Serializable
-sealed class KidTab : Route {
-    @Serializable
-    data object Journey : KidTab()
-
-    @Serializable
-    data object Mission : KidTab()
-
-    @Serializable
-    data object Wish : KidTab()
-}
-
-@Serializable
-sealed class KidRoute : Route {
-// TODO : KID 화면 내 세부화면 Route 설정
-}
