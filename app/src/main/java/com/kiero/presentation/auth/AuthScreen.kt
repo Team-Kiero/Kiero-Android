02d2dfecad9246adf6643f7b4e4fca0bb9624eed
Son Juwan <wndhks171@naver.com>
2026-01-07 17:17:22 +0900

    setting/#4: auth navigation 수정

--- app/src/main/java/com/kiero/presentation/auth/AuthScreen.kt
+++ app/src/main/java/com/kiero/presentation/auth/AuthScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
@@ -20,41 +19,31 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.kiero.core.common.extension.noRippleClickable
 import com.kiero.core.designsystem.theme.KieroTheme
 import com.kiero.core.model.UiState
-import com.kiero.data.auth.model.DummyEntity
-import com.kiero.presentation.auth.component.DummyItem
-import kotlinx.collections.immutable.PersistentList
 
 @Composable
 fun AuthRoute(
     paddingValues: PaddingValues,
-    snackbarHostState: SnackbarHostState,
     navigateUp: () -> Unit,
     navigateToParent: () -> Unit,
     navigateToKid: () -> Unit,
-    state: UiState<PersistentList<DummyEntity>>,
 ) {
     AuthScreen(
         paddingValues = paddingValues,
-        snackbarHostState = snackbarHostState,
         navigateUp = navigateUp,
         navigateToParent = navigateToParent,
         navigateToKid = navigateToKid,
-        state = state,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
     )
 }
 
 @Composable
 fun AuthScreen(
     paddingValues: PaddingValues,
-    snackbarHostState: SnackbarHostState,
     navigateUp: () -> Unit,
     navigateToParent: () -> Unit,
     navigateToKid: () -> Unit,
-    state: UiState<PersistentList<DummyEntity>>,
     modifier: Modifier = Modifier,
 ) {
     LazyColumn(
@@ -64,74 +53,36 @@ fun AuthScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        when (state) {
-            is UiState.Loading -> {
-                item {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 32.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        Text(
-                            textAlign = TextAlign.Center,
-                            text = "Kiero App",
-                            fontSize = 30.sp
-                        )
+        item {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 32.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    textAlign = TextAlign.Center,
+                    text = "Kiero App",
+                    fontSize = 30.sp
+                )
 
-                        Spacer(modifier = Modifier.height(40.dp))
+                Spacer(modifier = Modifier.height(40.dp))
 
-                        Button(
-                            onClick = navigateToParent,
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            Text(text = "부모 화면으로 이동")
-                        }
-
-                        Spacer(modifier = Modifier.height(16.dp))
-
-                        Button(
-                            onClick = navigateToKid,
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            Text(text = "아이 화면으로 이동")
-                        }
-                    }
+                Button(
+                    onClick = navigateToParent,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "부모 화면으로 이동")
                 }
-            }
 
-            is UiState.Empty -> {
-                item {
-                    Text(
-                        modifier = modifier
-                            .noRippleClickable { navigateUp() },
-                        textAlign = TextAlign.Center,
-                        text = "Dummy",
-                        fontSize = 30.sp
-                    )
-                }
-            }
+                Spacer(modifier = Modifier.height(16.dp))
 
-            is UiState.Failure -> {
-                item {
-                    Text(
-                        modifier = modifier
-                            .noRippleClickable { navigateUp() },
-                        textAlign = TextAlign.Center,
-                        text = state.message,
-                    )
-                }
-            }
-
-            is UiState.Success -> {
-                items(state.data) {
-                    DummyItem(
-                        id = it.id,
-                        firstName = it.firstName,
-                        lastName = it.lastName,
-                        profileUrl = it.profile,
-                    )
+                Button(
+                    onClick = navigateToKid,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "아이 화면으로 이동")
                 }
             }
         }
@@ -144,11 +95,9 @@ private fun DummyScreenPreview() {
     KieroTheme {
         AuthScreen(
             paddingValues = PaddingValues(),
-            snackbarHostState = SnackbarHostState(),
             navigateUp = {},
             navigateToParent = {},
-            navigateToKid = {},
-            state = UiState.Loading
+            navigateToKid = {}
         )
     }
 }
--- app/src/main/java/com/kiero/presentation/auth/navigation/AuthNavigation.kt
+++ app/src/main/java/com/kiero/presentation/auth/navigation/AuthNavigation.kt
@@ -1,36 +1,46 @@
 package com.kiero.presentation.auth.navigation
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.material3.SnackbarHostState
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import com.kiero.core.model.UiState
-import com.kiero.core.navigation.Auth
+import androidx.navigation.compose.navigation
+import com.kiero.core.navigation.AuthGraph
+import com.kiero.core.navigation.Route
 import com.kiero.presentation.auth.AuthRoute
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed interface Auth : Route
+
+@Serializable
+data object Login : Auth
 
 fun NavController.navigateToAuth(
     navOptions: NavOptions? = null,
 ) {
-    navigate(Auth.Login, navOptions)
+    navigate(Login, navOptions)
 }
 
 fun NavGraphBuilder.authNavGraph(
+    navController: NavHostController,
     paddingValues: PaddingValues,
-    snackbarHostState: SnackbarHostState,
     navigateUp: () -> Unit,
     navigateToParent: () -> Unit,
     navigateToKid: () -> Unit,
 ) {
-    composable<Auth.Login> {
-        AuthRoute(
-            paddingValues = paddingValues,
-            snackbarHostState = snackbarHostState,
-            navigateUp = navigateUp,
-            navigateToParent = navigateToParent,
-            navigateToKid = navigateToKid,
-            state = UiState.Loading
-        )
+    navigation<AuthGraph>(
+        startDestination = Login
+    ) {
+        composable<Login> {
+            AuthRoute(
+                paddingValues = paddingValues,
+                navigateUp = navigateUp,
+                navigateToParent = navigateToParent,
+                navigateToKid = navigateToKid,
+            )
+        }
     }
 }
