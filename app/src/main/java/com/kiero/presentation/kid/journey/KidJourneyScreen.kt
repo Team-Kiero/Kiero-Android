e2a9ceecfb619ee184f7de5e6af2bcd1af780593
minseong-PC <smsdia412@gmail.com>
2026-01-09 18:03:37 +0900

    feat/#10 sideeffect 선언

--- app/src/main/java/com/kiero/presentation/kid/journey/KidJourneyScreen.kt
+++ app/src/main/java/com/kiero/presentation/kid/journey/KidJourneyScreen.kt
@@ -8,16 +8,36 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.kiero.core.common.extension.collectSideEffect
 import com.kiero.core.designsystem.theme.KieroTheme
+import com.kiero.core.trigger.LocalGlobalUiEventTrigger
+import com.kiero.presentation.kid.journey.state.KidJourneySideEffect
+import com.kiero.presentation.kid.journey.viewmodel.KidJourneyViewModel
 
 @Composable
 fun KidJourneyRoute(
     paddingValues: PaddingValues,
     navigateUp: () -> Unit,
+    viewModel: KidJourneyViewModel = hiltViewModel()
 ) {
+    val globalTrigger = LocalGlobalUiEventTrigger.current
+
+    viewModel.sideEffect.collectSideEffect { sideEffect ->
+        when (sideEffect) {
+            is KidJourneySideEffect.ShowToast -> globalTrigger.showToast(sideEffect.message)
+            is KidJourneySideEffect.ShowSnackbar -> globalTrigger.showSnackbar(
+                sideEffect.message,
+                null
+            ) {}
+
+            KidJourneySideEffect.ShowDialog -> globalTrigger.dialogTrigger.show {}
+        }
+    }
+
     KidJourneyScreen(
         paddingValues = paddingValues,
-        navigateUp = navigateUp
+        navigateUp = navigateUp,
     )
 }
 
@@ -30,7 +50,7 @@ private fun KidJourneyScreen(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(paddingValues),
+            .padding(paddingValues)
     ) {
         Text(
             text = "오늘의 여정"
