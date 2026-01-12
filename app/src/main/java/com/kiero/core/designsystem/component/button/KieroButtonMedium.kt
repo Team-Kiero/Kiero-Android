281a13e990fe24b88ca4457f26b56a63a3174934
Kyu hyunSung <dmp100@naver.com>
2026-01-13 00:06:34 +0900

    refactor/#16 : 디자인 시스템 버튼 컴포넌트 리팩토링 및 피그마 수치 반영

--- app/src/main/java/com/kiero/core/designsystem/component/button/KieroButtonMedium.kt
+++ app/src/main/java/com/kiero/core/designsystem/component/button/KieroButtonMedium.kt
@@ -2,7 +2,6 @@ package com.kiero.core.designsystem.component.button
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,21 +10,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.kiero.core.designsystem.component.button.model.KieroButtonColors
+import com.kiero.core.designsystem.theme.KieroTheme
 import com.kiero.core.designsystem.theme.defaultKieroColors
 
 
@@ -34,41 +30,38 @@ fun KieroButtonMedium(
     text: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
-    leadingIcon: Painter? = null,
-    colors: KieroButtonColors = KieroButtonColors(),
+    leadingIcon: ImageVector? = null,
+    containerColor: Color = KieroTheme.colors.main,
+    contentColor: Color = KieroTheme.colors.black,
     isEnabled: Boolean = true,
 ) {
-    Button(
+    Surface(
         onClick = onClick,
-        modifier = modifier
-            .width(328.dp)
-            .height(46.dp),
         enabled = isEnabled,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(49.dp),
         shape = RoundedCornerShape(8.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = colors.containerColor(),
-            contentColor = colors.contentColor()
-        ),
-        contentPadding = PaddingValues(vertical = 11.dp)
+        color = if (isEnabled) containerColor else KieroTheme.colors.gray300,
+        contentColor = if (isEnabled) contentColor else KieroTheme.colors.gray600
     ) {
         Row(
+            modifier = Modifier
+                .padding(vertical = 13.dp, horizontal = 134.5.dp), // 내부 패딩으로 유연성 확보
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center
         ) {
             if (leadingIcon != null) {
                 Icon(
-                    painter = leadingIcon,
+                    imageVector = leadingIcon, // ImageVector로 변경
                     contentDescription = null,
-                    modifier = Modifier.size(20.dp),
-                    tint = colors.contentColor()
+                    modifier = Modifier.size(20.dp)
                 )
-
                 Spacer(modifier = Modifier.width(10.dp))
             }
-
             Text(
                 text = text,
-                style = TextStyle(fontSize = 15.sp, fontWeight = FontWeight.SemiBold)
+                style = KieroTheme.typography.semiBold.title3
             )
         }
     }
@@ -77,26 +70,30 @@ fun KieroButtonMedium(
 @Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
 @Composable
 private fun KieroButtonMediumPreview() {
-    Column(
-        modifier = Modifier
-            .padding(16.dp)
-            .fillMaxWidth(),
-        verticalArrangement = Arrangement.spacedBy(16.dp), // 버튼 사이 간격
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        KieroButtonMedium(
-            text = "시작하기",
-            onClick = { }
-        )
-
-        KieroButtonMedium(
-            text = "일정 추가하기",
-            leadingIcon = painterResource(id = com.kiero.R.drawable.ic_kid_camera),
-            colors = KieroButtonColors(
+    KieroTheme {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            KieroButtonMedium(
+                text = "시작하기",
+                onClick = { }
+            )
+            KieroButtonMedium(
+                text = "일정 추가하기",
+                leadingIcon = ImageVector.vectorResource(id = com.kiero.R.drawable.ic_kid_camera),
                 containerColor = defaultKieroColors.gray900,
-                contentColor = defaultKieroColors.white
-            ),
-            onClick = { }
-        )
+                contentColor = defaultKieroColors.white,
+                onClick = { }
+            )
+            KieroButtonMedium(
+                text = "비활성화 버튼",
+                isEnabled = false,
+                onClick = { }
+            )
+        }
     }
 }
--- app/src/main/java/com/kiero/core/designsystem/component/dialog/action/KieroConfirmAction.kt
+++ app/src/main/java/com/kiero/core/designsystem/component/dialog/action/KieroConfirmAction.kt
@@ -1,18 +1,10 @@
 package com.kiero.core.designsystem.component.dialog.action
 
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.kiero.core.designsystem.component.button.KieroButtonSmall
-import com.kiero.core.designsystem.component.button.model.KieroButtonColors
 import com.kiero.core.designsystem.theme.KieroTheme
 
 class KieroConfirmAction(
@@ -21,21 +13,17 @@ class KieroConfirmAction(
 ) : DialogAction {
     @Composable
     override fun invoke(modifier: Modifier) {
-        // 공통 컴포넌트 KieroButtonSmall로 교체
         KieroButtonSmall(
             text = text,
             onClick = onClick,
             modifier = modifier,
-            // 필요하다면 여기서 직접 colors를 주입해 다이얼로그 전용 색상을 쓸 수 있습니다.
-            colors = KieroButtonColors(
-                containerColor = KieroTheme.colors.main,
-                contentColor = Color.Black
-            )
+            containerColor = KieroTheme.colors.main,
+            contentColor = Color.Black
         )
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun KieroConfirmActionPreview() {
     KieroTheme {
