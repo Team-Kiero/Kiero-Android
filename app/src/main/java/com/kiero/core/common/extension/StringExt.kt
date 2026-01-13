d21a86bb19d68764285503c369e8c1186600ba26
Kyu hyunSung <dmp100@naver.com>
2026-01-13 15:44:35 +0900

    feat/#26 : Add toHighlightedText to StringExt

--- app/src/main/java/com/kiero/core/common/extension/StringExt.kt
+++ app/src/main/java/com/kiero/core/common/extension/StringExt.kt
@@ -1,5 +1,9 @@
 package com.kiero.core.common.extension
 
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
@@ -24,3 +28,31 @@ val String.formattedDeadLine: String
             this
         }
     }
+
+/**
+ * 특정 키워드에 하이라이트 색상을 입힌 AnnotatedString을 반환합니다.
+ * 여러 화면 뷰에서 하이라이트된 글자가 있어서 공통으로 뺐습니다.
+ * EX : 오늘의 여정 - "name": "피아노 학원 가기",
+ */
+fun String.toHighlightedText(
+    highlightColor: Color,
+    vararg highlightTexts: String? // null 대응을 위해 String? 사용
+): AnnotatedString = buildAnnotatedString {
+    append(this@toHighlightedText)
+
+    // 가변 인자(vararg)를 순회하며 스타일 적용
+    highlightTexts.forEach { target ->
+        if (target.isNullOrBlank()) return@forEach
+
+        var startIndex = indexOf(target)
+        while (startIndex >= 0) {
+            addStyle(
+                style = SpanStyle(color = highlightColor),
+                start = startIndex,
+                end = startIndex + target.length
+            )
+            // 다음 동일 키워드 검색 (중복 단어 대응)
+            startIndex = indexOf(target, startIndex + target.length)
+        }
+    }
+}
