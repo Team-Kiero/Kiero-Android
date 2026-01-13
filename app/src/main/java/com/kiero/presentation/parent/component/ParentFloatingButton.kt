5544faa1d23acdc7b2089055917abf8b686c0fba
Son Juwan <wndhks171@naver.com>
2026-01-14 02:38:15 +0900

    feat/#20: 부모 스케쥴 뷰 컴포넌트

---
+++ app/src/main/java/com/kiero/presentation/parent/component/ParentFloatingButton.kt
@@ -0,0 +1,45 @@
+package com.kiero.presentation.parent.component
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kiero.R
+import com.kiero.core.designsystem.theme.KieroTheme
+
+@Composable
+fun ParentFloatingButton(
+    buttonColor: Color,
+    onActiveClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    FloatingActionButton(
+        onClick = onActiveClick,
+        containerColor = buttonColor,
+        modifier = modifier
+            .size(53.dp),
+        shape = CircleShape,
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_parent_fab_plus),
+            contentDescription = null,
+            tint = Color.Unspecified
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ParentFloatingButtonPreview() {
+    ParentFloatingButton(
+        buttonColor = KieroTheme.colors.white,
+        onActiveClick = {}
+    )
+}
---
+++ app/src/main/java/com/kiero/presentation/parent/component/ParentTabRow.kt
@@ -0,0 +1,69 @@
+package com.kiero.presentation.parent.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.PrimaryTabRow
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.kiero.core.designsystem.theme.KieroTheme
+import com.kiero.presentation.parent.schedule.model.TabItem
+
+
+@Composable
+fun ParentTabRow(
+    modifier: Modifier = Modifier,
+    selectedTabIndex: Int,
+    onTabSelected: (Int) -> Unit,
+) {
+    val titles = TabItem.entries.map { it.title }
+
+    PrimaryTabRow(
+        selectedTabIndex = selectedTabIndex,
+        containerColor = KieroTheme.colors.gray900,
+        contentColor = KieroTheme.colors.white,
+        modifier = modifier
+            .fillMaxWidth(),
+        indicator = {
+            TabRowDefaults.PrimaryIndicator(
+                modifier = Modifier.tabIndicatorOffset(selectedTabIndex, matchContentSize = true),
+                width = Dp.Unspecified,
+                height = 1.dp,
+                color = KieroTheme.colors.white,
+                shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp),
+            )
+        },
+        divider = {}
+    ) {
+        titles.forEachIndexed { index, title ->
+            Tab(
+                selected = selectedTabIndex == index,
+                onClick = {
+                    onTabSelected(index)
+                },
+                text = {
+                    Text(
+                        text = title,
+                        style = KieroTheme.typography.bold.headLine2,
+                        color = if (selectedTabIndex == index) KieroTheme.colors.white else KieroTheme.colors.gray500
+                    )
+                },
+            )
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ParentTabRowPreview() {
+    ParentTabRow(
+        selectedTabIndex = 0,
+        onTabSelected = {}
+    )
+}
