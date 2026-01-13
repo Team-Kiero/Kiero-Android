69da37379c6a0ffd456002893f617463466b9011
Son Juwan <wndhks171@naver.com>
2026-01-14 02:39:56 +0900

    feat/#20: 스케쥴 navigation 및 컴포넌트

---
+++ app/src/main/java/com/kiero/presentation/parent/schedule/plan/navigation/ScheduleAddNavigation.kt
@@ -0,0 +1,32 @@
+package com.kiero.presentation.parent.schedule.plan.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.kiero.core.navigation.Route
+import com.kiero.presentation.parent.navigation.Schedule
+import com.kiero.presentation.parent.schedule.screen.ParentScheduleAddRoute
+import kotlinx.serialization.Serializable
+
+fun NavController.navigateToScheduleAdd(
+    navOptions: NavOptions? = null,
+) {
+    navigate(ScheduleAdd, navOptions)
+}
+
+fun NavGraphBuilder.parentScheduleAddNavGraph(
+    paddingValues: PaddingValues,
+    navigateUp: () -> Unit,
+) {
+    composable<Schedule> {
+        ParentScheduleAddRoute(
+            paddingValues = paddingValues,
+            navigateUp = navigateUp,
+        )
+    }
+}
+
+@Serializable
+data object ScheduleAdd : Route
