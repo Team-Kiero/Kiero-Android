e639643edfc4ce2816e3396cc778e5a080d92c47
Son Juwan <wndhks171@naver.com>
2026-01-16 08:13:20 +0900

    feat/#36: Auth/kid 회원가입 model

--- app/src/main/java/com/kiero/presentation/auth/kid/KidSignupViewModel.kt
+++ app/src/main/java/com/kiero/presentation/auth/kid/KidSignupViewModel.kt
@@ -2,8 +2,6 @@ package com.kiero.presentation.auth.kid
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.kiero.presentation.auth.model.AuthSideEffect
-import com.kiero.presentation.auth.model.AuthState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -19,19 +17,20 @@ class KidSignupViewModel @Inject constructor() : ViewModel() {
     private val _state = MutableStateFlow(KidState())
     val state: StateFlow<KidState> = _state.asStateFlow()
 
-    private val _sideEffect = MutableSharedFlow< KidSideEffect>()
+    private val _sideEffect = MutableSharedFlow<KidSideEffect>()
     val sideEffect = _sideEffect.asSharedFlow()
 
     fun onSignupClick(
         firstName: String,
         lastName: String,
-        inviteCode: String
+        inviteCode: String,
     ) {
         viewModelScope.launch {
             when {
                 firstName.isBlank() && lastName.isBlank() && inviteCode.isBlank() -> {
                     _sideEffect.emit(KidSideEffect.ShowSnackBar("이름이나 초대 코드를 확인해줘."))
                 }
+
                 else -> {
                     // TODO : 테스트 후 고치기.
                 }
