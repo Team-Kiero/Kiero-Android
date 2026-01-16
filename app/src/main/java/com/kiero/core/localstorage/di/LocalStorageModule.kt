a5ac57f42b0ae1f3c84f50653bab49d61a81c86f
sonms <smsms5676@naver.com>
2026-01-17 04:38:26 +0900

    add/#30 확장 함수 추가

---
+++ app/src/main/java/com/kiero/core/localstorage/di/LocalStorageModule.kt
@@ -0,0 +1,36 @@
+package com.kiero.core.localstorage.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.kiero.core.localstorage.TokenManager
+import com.kiero.core.localstorage.TokenManagerImpl
+import com.kiero.core.security.CryptoManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object LocalStorageModule {
+    private val Context.authDataStore: DataStore<Preferences> by preferencesDataStore(name = "kiero_auth_datastore")
+
+    @Provides
+    @Singleton
+    fun provideAuthDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return context.authDataStore
+    }
+
+    @Provides
+    @Singleton
+    fun provideTokenManager(
+        dataStore: DataStore<Preferences>,
+        cryptoManager: CryptoManager
+    ): TokenManager {
+        return TokenManagerImpl(dataStore, cryptoManager)
+    }
+}
