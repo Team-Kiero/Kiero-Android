97136b688ed8f4234e6d5d775c759a2d28173fcf
Kyu hyunSung <dmp100@naver.com>
2026-01-17 04:34:54 +0900

    refactor/#34 : 위치조정을 위한 SnackbarState에 Dp 추가

--- app/src/main/java/com/kiero/core/model/trigger/SnackbarState.kt
+++ app/src/main/java/com/kiero/core/model/trigger/SnackbarState.kt
@@ -1,8 +1,11 @@
 package com.kiero.core.model.trigger
 
 import androidx.compose.runtime.Immutable
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 @Immutable
 data class SnackbarState(
     val message: String = "",
+    val bottomPadding: Dp = 0.dp,
 )
