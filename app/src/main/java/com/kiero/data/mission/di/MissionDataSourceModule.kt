c446e630c5fc396679533f95b3e9c2fb43141a18
sonms <smsms5676@naver.com>
2026-01-17 04:40:31 +0900

    delete/#30 사용하지 않는 import 문 삭제

--- app/src/main/java/com/kiero/data/mission/di/MissionDataSourceModule.kt
+++ app/src/main/java/com/kiero/data/mission/di/MissionDataSourceModule.kt
@@ -1,14 +1,8 @@
 package com.kiero.data.mission.di
 
-import com.kiero.data.auth.local.datasource.AuthLocalDataSource
-import com.kiero.data.auth.local.datasourceimpl.AuthLocalDataSourceImpl
-import com.kiero.data.auth.remote.datasource.AuthDataSource
-import com.kiero.data.auth.remote.datasourceimpl.AuthDataSourceImpl
-import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
--- app/src/main/java/com/kiero/presentation/auth/AuthScreen.kt
+++ app/src/main/java/com/kiero/presentation/auth/AuthScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -59,7 +58,6 @@ fun AuthScreen(
         Image(
             painter = logoPainter,
             contentDescription = null,
-            contentScale = ContentScale.None,
             modifier = Modifier.forcePixelToDp(logoPainter)
         )
 
