7e2841185acaf7e3eef08d09417643518de166f3
seungjae708 <choesj365@gmail.com>
2026-01-18 22:22:22 +0900

    feat/#45: 오늘의 여정 조회 toUiModel 구현

--- app/src/main/java/com/kiero/presentation/kid/journey/model/KidJourneyHeaderUiModel.kt
+++ app/src/main/java/com/kiero/presentation/kid/journey/model/KidJourneyHeaderUiModel.kt
@@ -1,13 +1,23 @@
 package com.kiero.presentation.kid.journey.model
 
 import androidx.compose.runtime.Immutable
-import java.time.LocalDate
+import com.kiero.data.schedule.model.TodayScheduleModel
 
 @Immutable
 data class KidJourneyHeaderUiModel(
     val kidName: String,
-    val currentDate: LocalDate,
+    val currentDate: String,
     val coinCount: Int,
     val earnedStones: Int,
     val totalScheduleCount: Int
 )
+
+fun TodayScheduleModel.toUiModel() : KidJourneyHeaderUiModel {
+    return KidJourneyHeaderUiModel(
+        kidName = "주완",
+        currentDate = "12월 5일 목요일",
+        coinCount = 350,
+        earnedStones = earnedStones ?: 0,
+        totalScheduleCount = totalSchedule
+    )
+}
