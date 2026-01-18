add49388849fd707574451c8602df3be86598474
seungjae708 <choesj365@gmail.com>
2026-01-18 22:20:07 +0900

    feat/#45: 오늘의 여정 조회 dto 구현

---
+++ app/src/main/java/com/kiero/data/schedule/dto/response/TodayScheduleResponseDto.kt
@@ -0,0 +1,30 @@
+package com.kiero.data.schedule.dto.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class TodayScheduleResponseDto(
+    @SerialName("scheduleDetailId")
+    val scheduleDetailId: Long?,
+    @SerialName("scheduleOrder")
+    val scheduleOrder: Int,
+    @SerialName("startTime")
+    val startTime: String?,
+    @SerialName("endTime")
+    val endTime: String?,
+    @SerialName("name")
+    val name: String?,
+    @SerialName("stoneType")
+    val stoneType: String?,
+    @SerialName("totalSchedule")
+    val totalSchedule: Int,
+    @SerialName("earnedStones")
+    val earnedStones: Int?,
+    @SerialName("scheduleStatus")
+    val scheduleStatus: String,
+    @SerialName("isSkippable")
+    val isSkippable: Boolean,
+    @SerialName("isNowScheduleVerified")
+    val isNowScheduleVerified: Boolean
+)
