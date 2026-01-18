3f4c0f480ac425e00034f22a84b03336590584d9
seungjae708 <choesj365@gmail.com>
2026-01-19 02:10:09 +0900

    feat/#45: 일정 인증 이미지 presigned-url 발급, 오늘의 일정 단일 완료 repository 구현

--- app/src/main/java/com/kiero/data/schedule/repository/ScheduleRepository.kt
+++ app/src/main/java/com/kiero/data/schedule/repository/ScheduleRepository.kt
@@ -1,7 +1,18 @@
 package com.kiero.data.schedule.repository
 
+import com.kiero.data.schedule.model.ScheduleImageUploadModel
 import com.kiero.data.schedule.model.TodayScheduleModel
 
 interface ScheduleRepository {
     suspend fun getTodaySchedule(): Result<TodayScheduleModel>
+
+    suspend fun getPresignedUrl(
+        fileName: String,
+        contentType: String
+    ): Result<ScheduleImageUploadModel>
+
+    suspend fun postScheduleComplete(
+        scheduleDetailId: Long,
+        imageUrl: String
+    ): Result<Unit>
 }
--- app/src/main/java/com/kiero/data/schedule/repositoryimpl/ScheduleRepositoryImpl.kt
+++ app/src/main/java/com/kiero/data/schedule/repositoryimpl/ScheduleRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.kiero.data.schedule.repositoryimpl
 
 import com.kiero.core.common.util.suspendRunCatching
 import com.kiero.data.schedule.datasource.ScheduleDataSource
+import com.kiero.data.schedule.model.ScheduleImageUploadModel
 import com.kiero.data.schedule.model.TodayScheduleModel
 import com.kiero.data.schedule.model.toModel
 import com.kiero.data.schedule.repository.ScheduleRepository
@@ -14,4 +15,23 @@ class ScheduleRepositoryImpl @Inject constructor(
         dataSource.getTodaySchedule().data!!.toModel()
     }
 
+    override suspend fun getPresignedUrl(
+        fileName: String,
+        contentType: String
+    ): Result<ScheduleImageUploadModel> = suspendRunCatching {
+        dataSource.getPresignedUrl(
+            fileName = fileName,
+            contentType = contentType
+        ).data!!.toModel()
+    }
+
+    override suspend fun postScheduleComplete(
+        scheduleDetailId: Long,
+        imageUrl: String
+    ): Result<Unit> = suspendRunCatching {
+        dataSource.postScheduleComplete(
+            scheduleDetailId = scheduleDetailId,
+            imageUrl = imageUrl
+        ).data!!
+    }
 }
