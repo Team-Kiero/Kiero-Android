1d513202ed3924057f229628a2ec017a99d4fc7c
seungjae708 <choesj365@gmail.com>
2026-01-18 23:54:29 +0900

    feat/#45: viewModel 구현(데이터 오는지 확인 필요)

--- app/src/main/java/com/kiero/presentation/kid/journey/viewmodel/KidJourneyViewModel.kt
+++ app/src/main/java/com/kiero/presentation/kid/journey/viewmodel/KidJourneyViewModel.kt
@@ -2,6 +2,12 @@ package com.kiero.presentation.kid.journey.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.kiero.data.kid.coin.repository.CoinRepository
+import com.kiero.data.schedule.model.ScheduleStatus
+import com.kiero.data.schedule.repository.ScheduleRepository
+import com.kiero.presentation.kid.journey.model.KidJourneyContentUiModel
+import com.kiero.presentation.kid.journey.model.KidJourneyHeaderUiModel
+import com.kiero.presentation.kid.journey.model.KidJourneyScheduleUiModel
 import com.kiero.presentation.kid.journey.state.KidJourneySideEffect
 import com.kiero.presentation.kid.journey.state.KidJourneyState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,20 +17,104 @@ import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class KidJourneyViewModel @Inject constructor(
+    private val repository: ScheduleRepository,
+    private val coinRepository: CoinRepository
 ) : ViewModel() {
+    private val coin = coinRepository.myCoin
+
     private val _state = MutableStateFlow(KidJourneyState.fake())
     val state = _state.asStateFlow()
 
     private val _sideEffect = MutableSharedFlow<KidJourneySideEffect>()
     val sideEffect: SharedFlow<KidJourneySideEffect> = _sideEffect.asSharedFlow()
 
+    init {
+        fetchCoin()
+        fetchTodaySchedule()
+    }
+
+    private fun fetchTodaySchedule() {
+        viewModelScope.launch {
+            repository.getTodaySchedule()
+                .onSuccess { scheduleData ->
+                    _state.value = _state.value.copy(
+                        header = KidJourneyHeaderUiModel(
+                            kidName = coin.value.firstName,
+                            currentDate = coin.value.today,
+                            coinCount = coin.value.coinAmount,
+                            earnedStones = scheduleData.earnedStones,
+                            totalScheduleCount = scheduleData.totalSchedule
+                        ),
+                        content = when (scheduleData.scheduleStatus) {
+                            ScheduleStatus.NO_SCHEDULE -> KidJourneyContentUiModel.NoSchedule
+
+                            ScheduleStatus.FIRST_SCHEDULE -> KidJourneyContentUiModel.FirstSchedule(
+                                scheduleName = scheduleData.name,
+                                scheduleInfo = KidJourneyScheduleUiModel(
+                                    order = scheduleData.scheduleOrder,
+                                    startTime = scheduleData.startTime,
+                                    endTime = scheduleData.endTime
+                                ),
+                                isSkippable = scheduleData.isSkippable
+                            )
+
+                            ScheduleStatus.NOW_SCHEDULE_EXIST -> KidJourneyContentUiModel.NowSchedule(
+                                scheduleName = scheduleData.name,
+                                stoneType = scheduleData.stoneType,
+                                scheduleInfo = KidJourneyScheduleUiModel(
+                                    order = scheduleData.scheduleOrder,
+                                    startTime = scheduleData.startTime,
+                                    endTime = scheduleData.endTime
+                                ),
+                                isSkippable = scheduleData.isSkippable
+                            )
+
+                            ScheduleStatus.NEXT_SCHEDULE_EXIST -> KidJourneyContentUiModel.NextSchedule(
+                                scheduleName = scheduleData.name,
+                                stoneType = scheduleData.stoneType,
+                                scheduleInfo = KidJourneyScheduleUiModel(
+                                    order = scheduleData.scheduleOrder,
+                                    startTime = scheduleData.startTime,
+                                    endTime = scheduleData.endTime
+                                ),
+                                isSkippable = scheduleData.isSkippable
+                            )
+
+                            ScheduleStatus.FIRE_NOT_LIT -> KidJourneyContentUiModel.FireNotLit(
+                                kidName = coin.value.firstName
+                            )
+
+                            ScheduleStatus.FIRE_LIT -> KidJourneyContentUiModel.FireLit
+                        }
+                    )
+                    Timber.d("fetchTodaySchedule: $scheduleData")
+                }
+                .onFailure {
+                    Timber.e("fetchTodaySchedule fail: $it")
+                }
+        }
+    }
+
     fun onNextClick() {
         viewModelScope.launch {
             _sideEffect.emit(KidJourneySideEffect.ShowDialog)
+        }
+    }
+
+    fun fetchCoin() {
+        viewModelScope.launch {
+            coinRepository.getCurrentCoin()
+                .onSuccess {
+                    Timber.d("fetchCoin: $it")
+                }
+                .onFailure {
+                    Timber.e("fetchCoin fail: $it")
+                }
         }
     }
 }
