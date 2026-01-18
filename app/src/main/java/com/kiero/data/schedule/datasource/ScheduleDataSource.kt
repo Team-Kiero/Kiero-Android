5aa4f55566cedb5838b543625e3ead441a8d7551
seungjae708 <choesj365@gmail.com>
2026-01-19 02:09:52 +0900

    feat/#45: 일정 인증 이미지 presigned-url 발급, 오늘의 일정 단일 완료 dataSource 구현

--- app/src/main/java/com/kiero/data/schedule/datasource/ScheduleDataSource.kt
+++ app/src/main/java/com/kiero/data/schedule/datasource/ScheduleDataSource.kt
@@ -1,8 +1,19 @@
 package com.kiero.data.schedule.datasource
 
 import com.kiero.core.network.model.BaseResponse
+import com.kiero.data.schedule.dto.response.ScheduleImageUploadResponseDto
 import com.kiero.data.schedule.dto.response.TodayScheduleResponseDto
 
 interface ScheduleDataSource {
     suspend fun getTodaySchedule(): BaseResponse<TodayScheduleResponseDto>
+
+    suspend fun getPresignedUrl(
+        fileName: String,
+        contentType: String
+    ): BaseResponse<ScheduleImageUploadResponseDto>
+
+    suspend fun postScheduleComplete(
+        scheduleDetailId: Long,
+        imageUrl: String
+    ): BaseResponse<Unit>
 }
--- app/src/main/java/com/kiero/data/schedule/datasourceimpl/ScheduleDataSourceImpl.kt
+++ app/src/main/java/com/kiero/data/schedule/datasourceimpl/ScheduleDataSourceImpl.kt
@@ -3,6 +3,9 @@ package com.kiero.data.schedule.datasourceimpl
 import com.kiero.core.network.model.BaseResponse
 import com.kiero.data.schedule.api.ScheduleService
 import com.kiero.data.schedule.datasource.ScheduleDataSource
+import com.kiero.data.schedule.dto.request.ScheduleCompleteRequestDto
+import com.kiero.data.schedule.dto.request.ScheduleImageUploadRequestDto
+import com.kiero.data.schedule.dto.response.ScheduleImageUploadResponseDto
 import com.kiero.data.schedule.dto.response.TodayScheduleResponseDto
 import javax.inject.Inject
 
@@ -11,4 +14,26 @@ class ScheduleDataSourceImpl @Inject constructor(
 ) : ScheduleDataSource {
     override suspend fun getTodaySchedule(): BaseResponse<TodayScheduleResponseDto> =
         service.getTodaySchedule()
+
+    override suspend fun getPresignedUrl(
+        fileName: String,
+        contentType: String
+    ): BaseResponse<ScheduleImageUploadResponseDto> =
+        service.getPresignedUrl(
+            request = ScheduleImageUploadRequestDto(
+                fileName = fileName,
+                contentType = contentType
+            )
+        )
+
+    override suspend fun postScheduleComplete(
+        scheduleDetailId: Long,
+        imageUrl: String
+    ): BaseResponse<Unit> =
+        service.postScheduleComplete(
+            scheduleDetailId = scheduleDetailId,
+            request = ScheduleCompleteRequestDto(
+                imageUrl = imageUrl
+            )
+        )
 }
