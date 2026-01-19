7c83fc5722c8509e2f761ea59861efa1f9f068ef
sonms <smsms5676@naver.com>
2026-01-20 00:44:44 +0900

    feat#48 입력값 공백 제거 로직 추가
    - `MaxLengthInputTransformation`에 입력된 텍스트의 모든 공백을 제거하는 로직을 추가했습니다.

--- app/src/main/java/com/kiero/core/common/util/MaxLengthInputTransformation.kt
+++ app/src/main/java/com/kiero/core/common/util/MaxLengthInputTransformation.kt
@@ -8,6 +8,15 @@ data class MaxLengthInputTransformation(
     private val limit: Int
 ) : InputTransformation {
     override fun TextFieldBuffer.transformInput() {
+        var i = 0
+        while (i < length) {
+            if (asCharSequence()[i].isWhitespace()) {
+                delete(i, i + 1)
+            } else {
+                i++
+            }
+        }
+
         if (length > limit) {
             delete(limit, length)
         }
