a2f4d3e672443a0c867e6ced92eab4d5779996ee
seungjae708 <choesj365@gmail.com>
2026-01-20 03:42:35 +0900

    feat/#45: ScheduleFireResponseDto 추가

---
+++ app/src/main/java/com/kiero/data/schedule/dto/response/ScheduleFireResponseDto.kt
@@ -0,0 +1,12 @@
+package com.kiero.data.schedule.dto.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ScheduleFireResponseDto (
+    @SerialName("gotStones")
+    val gotStones: List<String>,
+    @SerialName("earnedCoinAmount")
+    val earnedCoinAmount: Int
+)
