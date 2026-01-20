d92a016a83cc6e66b3e3f0e848539828a22ca58e
sonms <smsms5676@naver.com>
2026-01-20 19:37:08 +0900

    feat#52: API 수정 및 UI 개선
    - `MissionService`의 `completeMission` API에서 `@Query`를 `@Path`로 수정
    - `WishService`의 `patchCoupon` API의 HTTP 메서드를 `POST`에서 `PATCH`로 변경
    - `MainScreen`의 스낵바 하단 패딩 값을 90.dp에서 30.dp로 수정
    - `KieroDialog`의 제목과 부제목 텍스트를 중앙 정렬하도록 수정
    - `UiState.Success`에서 데이터를 쉽게 추출할 수 있는 `successData` 확장 프로퍼티 추가

--- app/src/main/java/com/kiero/data/kid/wish/remote/api/WishService.kt
+++ app/src/main/java/com/kiero/data/kid/wish/remote/api/WishService.kt
@@ -3,14 +3,14 @@ package com.kiero.data.kid.wish.remote.api
 import com.kiero.core.network.model.BaseResponse
 import com.kiero.data.kid.wish.remote.dto.response.WishResponseDto
 import retrofit2.http.GET
-import retrofit2.http.POST
+import retrofit2.http.PATCH
 import retrofit2.http.Path
 
 interface WishService {
     @GET("api/v1/coupons")
     suspend fun getCoupons() : BaseResponse<List<WishResponseDto>>
 
-    @POST("api/v1/coupons/{couponId}")
+    @PATCH("api/v1/coupons/{couponId}")
     suspend fun patchCoupon(
         @Path("couponId") couponId: Long,
     ) : BaseResponse<WishResponseDto>
--- app/src/main/java/com/kiero/presentation/main/screen/MainScreen.kt
+++ app/src/main/java/com/kiero/presentation/main/screen/MainScreen.kt
@@ -184,7 +184,7 @@ fun MainScreen(
                             // TODO: 디자인 확정 후 스낵바 높이 및 패딩 수정 필요
                             modifier = Modifier
                                 .padding(horizontal = 16.dp)
-                                .padding(bottom = 90.dp)
+                                .padding(bottom = 30.dp)
                         )
                     }
                 },
