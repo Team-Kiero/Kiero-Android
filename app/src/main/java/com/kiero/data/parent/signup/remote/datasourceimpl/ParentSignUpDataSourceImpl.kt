00a8ac3f43e70bd75b45f1853790d4be671c0ccd
Son Juwan <wndhks171@naver.com>
2026-02-19 13:52:20 +0900

    add/#91: data/parent/signup datasourceimpl 오타 수정

--- app/src/main/java/com/kiero/data/parent/signup/remote/datasourceimpl/ParentSignUpDataSourceImpl.kt
+++ app/src/main/java/com/kiero/data/parent/signup/remote/datasourceimpl/ParentSignUpDataSourceImpl.kt
@@ -1,11 +1,11 @@
-package com.kiero.data.parent.signup.remote.datasourcimpl
+package com.kiero.data.parent.signup.remote.datasourceimpl
 
 import com.kiero.core.network.model.BaseResponse
-import com.kiero.data.parent.signup.remote.dto.response.ParentSignUpResponseDto
 import com.kiero.data.parent.signup.remote.api.ParentSignUpService
 import com.kiero.data.parent.signup.remote.datasource.ParentSignUpDataSource
 import com.kiero.data.parent.signup.remote.dto.reqeust.ParentSignUpRequestDto
 import com.kiero.data.parent.signup.remote.dto.response.LinkageKidResponseDto
+import com.kiero.data.parent.signup.remote.dto.response.ParentSignUpResponseDto
 import javax.inject.Inject
 
 class ParentSignUpDataSourceImpl @Inject constructor(
