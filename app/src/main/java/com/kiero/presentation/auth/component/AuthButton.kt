6bde9160d330fabf7d5ef6035fd641405791235a
sonms <smsms5676@naver.com>
2026-03-04 00:53:38 +0900

    mod/#104: auth 버튼 수정

--- app/src/main/java/com/kiero/presentation/auth/component/AuthButton.kt
+++ app/src/main/java/com/kiero/presentation/auth/component/AuthButton.kt
@@ -1,14 +1,13 @@
 package com.kiero.presentation.auth.component
 
-import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
@@ -16,44 +15,44 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.kiero.core.common.extension.forcePixelToDp
+import com.kiero.R
 import com.kiero.core.designsystem.theme.KieroTheme
 
 @Composable
 fun AuthButton(
-    @DrawableRes icon: Int,
     text : String,
     onClickButton: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val painter = painterResource(id = icon)
     val interactionSource = remember { MutableInteractionSource() }
 
     Row(
         modifier = modifier
-            .clip(RoundedCornerShape(10.dp))
+            .clip(RoundedCornerShape(20.dp))
             .background(
                 color = KieroTheme.colors.gray900,
-                shape = RoundedCornerShape(10.dp)
+                shape = RoundedCornerShape(20.dp)
             )
             .clickable(
                 interactionSource = interactionSource,
                 indication = ripple(color = KieroTheme.colors.white),
                 onClick = onClickButton
             )
-            .padding(vertical = 16.dp, horizontal = 29.dp),
-        verticalAlignment = Alignment.CenterVertically
+            .padding(vertical = 29.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        Image(
-            painter = painter,
+        Icon(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_parent_addschedule_check_off),
             contentDescription = null,
-            modifier = Modifier
-                .size(50.dp)
-                .forcePixelToDp(painter)
+            tint = Color.Unspecified,
+            modifier = Modifier.padding(start = 10.dp)
         )
 
         Text(
@@ -61,7 +60,7 @@ fun AuthButton(
             style = KieroTheme.typography.regular.body2,
             color = KieroTheme.colors.white,
             textAlign = TextAlign.Center,
-            modifier = Modifier.weight(1f)
+            modifier = Modifier.padding(end = 34.dp)
         )
     }
 }
@@ -71,8 +70,7 @@ fun AuthButton(
 private fun AuthButtonPreview() {
     KieroTheme {
         AuthButton(
-            text = "부모님으로 시작하기",
-            icon = com.kiero.R.drawable.img_auth_parent_goblin,
+            text = "부모님으로",
             onClickButton = {}
         )
     }
