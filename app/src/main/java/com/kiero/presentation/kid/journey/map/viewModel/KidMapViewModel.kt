e6e5b807a270c30acdbd36048e15a708c76667c2
seungjae708 <choesj365@gmail.com>
2026-03-05 01:49:18 +0900

    feat/#101: `KidMapScreen` 내 여정 리스트 UI 구현 및 `KidMapEmptyView` 추가

--- app/src/main/java/com/kiero/presentation/kid/journey/map/viewModel/KidMapViewModel.kt
+++ app/src/main/java/com/kiero/presentation/kid/journey/map/viewModel/KidMapViewModel.kt
@@ -3,8 +3,13 @@ package com.kiero.presentation.kid.journey.map.viewModel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.navigation.toRoute
+import com.kiero.core.model.UiState
 import com.kiero.presentation.kid.journey.map.navigation.Map
+import com.kiero.presentation.kid.journey.map.state.KidMapState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,5 +18,11 @@ class KidMapViewModel @Inject constructor(
 ) : ViewModel() {
     private val map = savedStateHandle.toRoute<Map>()
 
-    val date: String = map.date
+    private val _state = MutableStateFlow<UiState<KidMapState>>(
+        UiState.Success(
+            KidMapState.FAKE().copy(date = map.date)
+        )
+    )
+    val state: StateFlow<UiState<KidMapState>> = _state.asStateFlow()
+
 }
