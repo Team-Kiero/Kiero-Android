8088dd8e6a7b4c5547f7c1c2d737744c6238162b
minseong-PC <smsdia412@gmail.com>
2026-03-04 10:44:56 +0900

    delete/#104 사용하지 않는 이벤트 제거

--- app/src/main/java/com/kiero/presentation/main/navigation/KieroNavHost.kt
+++ app/src/main/java/com/kiero/presentation/main/navigation/KieroNavHost.kt
@@ -73,7 +73,6 @@ fun KieroNavHost(
                 )
             },
             navigateToSelection = appState::navigateToSelection,
-            onEasterEggClick = appState::navigateToKidOnboarding
         )
 
         parentSignUpNavGraph(
