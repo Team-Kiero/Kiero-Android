5ab6264ec6b83ad5e5890a266fcb038bf8b7e50e
seungjae708 <choesj365@gmail.com>
2026-03-08 23:19:46 +0900

    refactor/#101: `KidFireUiModel` 내 `earnedStones`를 `ImmutableList`로 변경

--- app/src/main/java/com/kiero/presentation/kid/journey/fire/state/KidFireResultContract.kt
+++ app/src/main/java/com/kiero/presentation/kid/journey/fire/state/KidFireResultContract.kt
@@ -3,6 +3,7 @@ package com.kiero.presentation.kid.journey.fire.state
 import androidx.compose.runtime.Immutable
 import com.kiero.presentation.kid.journey.fire.model.KidFireUiModel
 import com.kiero.presentation.kid.journey.model.KidJourneyStoneType
+import kotlinx.collections.immutable.persistentListOf
 
 @Immutable
 data class KidFireResultState(
@@ -12,7 +13,7 @@ data class KidFireResultState(
     companion object{
         val FAKE = KidFireResultState(
             content = KidFireUiModel(
-                earnedStones = listOf(
+                earnedStones = persistentListOf(
                     KidJourneyStoneType.WISDOM,
                     KidJourneyStoneType.COURAGE,
                     KidJourneyStoneType.GRIT
