a2fa8a366dfaecb6fbaf45eb2b7d5859371afa88
sonms <smsms5676@naver.com>
2026-03-10 17:21:41 +0900

    feat/#96: 바텀 바의 스크롤 소비를 자식에서 끝내도록 하는 확장함수 추가

--- app/src/main/java/com/kiero/core/common/extension/ModifierExt.kt
+++ app/src/main/java/com/kiero/core/common/extension/ModifierExt.kt
@@ -24,6 +24,9 @@ import androidx.compose.ui.graphics.drawOutline
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -98,4 +101,26 @@ fun Modifier.statusBarColor(backgroundColor: Color): Modifier = composed {
             size = Size(size.width, statusBarHeight.toPx())
         )
     }
+}
+
+fun Modifier.disableNestedScroll(): Modifier = composed {
+    val connection = remember {
+        object : NestedScrollConnection {
+            override fun onPreScroll(
+                available: Offset,
+                source: NestedScrollSource
+            ): Offset {
+                return Offset.Zero
+            }
+
+            override fun onPostScroll(
+                consumed: Offset,
+                available: Offset,
+                source: NestedScrollSource
+            ): Offset {
+                return available
+            }
+        }
+    }
+    this.nestedScroll(connection)
 }
