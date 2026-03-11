872f80f282a95c2b3f5d924c12c72b403f1c0cb1
sonms <smsms5676@naver.com>
2026-03-11 23:47:40 +0900

    feat/#96: 이미지 클릭 시 나오는 다이얼로그 구현

--- app/src/main/java/com/kiero/presentation/parent/screen/journey/ParentJourneyScreen.kt
+++ app/src/main/java/com/kiero/presentation/parent/screen/journey/ParentJourneyScreen.kt
@@ -131,7 +131,7 @@ private fun ParentJourneyScreen(
                     ParentJourneyTodayStatusItem(
                         item = item,
                         onItemClick = {
-                            isBottomSheetVisible = true
+                            isTodayJourneyVisible = true
                         }
                     )
                 }
