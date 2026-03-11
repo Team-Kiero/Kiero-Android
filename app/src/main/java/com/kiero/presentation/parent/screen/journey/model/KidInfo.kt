82e461873df9560e4e5ed4e2427950ed4de9e51d
minseong-PC <smsdia412@gmail.com>
2026-03-11 10:37:50 +0900

    mod/#104 stable하도록 수정

--- app/src/main/java/com/kiero/presentation/parent/screen/journey/model/KidInfo.kt
+++ app/src/main/java/com/kiero/presentation/parent/screen/journey/model/KidInfo.kt
@@ -1,12 +1,13 @@
 package com.kiero.presentation.parent.screen.journey.model
 
+import com.kiero.core.common.util.toDotSeparatedDate
 import com.kiero.data.auth.model.ChildrenModel
 import java.time.LocalDate
 
 data class KidInfo(
     val kidId: String = "",
     val kidName: String = "",
-    val currentDate: LocalDate = LocalDate.now()
+    val currentDate: String = LocalDate.now().toDotSeparatedDate()
 )
 
 fun ChildrenModel.toUiModel() = KidInfo(
