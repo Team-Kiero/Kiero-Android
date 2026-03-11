bafbb57941701ade72fe84a662126c8c60986399
minseong-PC <smsdia412@gmail.com>
2026-03-11 10:37:29 +0900

    feat/#104 sse manager 세팅

--- app/src/main/java/com/kiero/data/sse/model/SseEvent.kt
+++ app/src/main/java/com/kiero/data/sse/model/SseEvent.kt
@@ -3,27 +3,22 @@ package com.kiero.data.sse.model
 import com.kiero.data.sse.remote.dto.response.FeedDataDto
 import com.kiero.data.sse.remote.dto.response.InviteDataDto
 import com.kiero.data.sse.remote.dto.response.MissionDataDto
+import com.kiero.data.sse.remote.dto.response.ParentScheduleDataDto
 import com.kiero.data.sse.remote.dto.response.ScheduleDataDto
 
 sealed interface SseEvent {
-    // 공통
     data object Connected : SseEvent
 
-    // 부모 전용 이벤트
-    data class Invite(
-        val data: InviteDataDto
-    ) : SseEvent
+    // 부모 전용
+    sealed interface Parent : SseEvent {
+        data class Invite(val data: InviteDataDto) : Parent
+        data class Feed(val data: FeedDataDto) : Parent
+        data class Schedule(val data: ParentScheduleDataDto) : Parent
+    }
 
-    data class Feed(
-        val data: FeedDataDto
-    ) : SseEvent
-
-    // 자녀 전용 이벤트
-    data class Mission(
-        val data: MissionDataDto
-    ) : SseEvent
-
-    data class Schedule(
-        val data: ScheduleDataDto
-    ) : SseEvent
+    // 자녀 전용
+    sealed interface Kid : SseEvent {
+        data class Mission(val data: MissionDataDto) : Kid
+        data class Schedule(val data: ScheduleDataDto) : Kid
+    }
 }
