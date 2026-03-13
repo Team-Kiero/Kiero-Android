2532e324070bb79b9b252ab0c532300b6b1f847d
seungjae708 <choesj365@gmail.com>
2026-03-13 14:36:22 +0900

    refactor/#101: navigateToParentSignUp 파라미터 수정 (이름, 프로필 이미지 제거)

--- app/src/main/java/com/kiero/presentation/main/navigation/MainAppState.kt
+++ app/src/main/java/com/kiero/presentation/main/navigation/MainAppState.kt
@@ -140,12 +140,8 @@ class MainAppState(
     )
 
     fun navigateToParentSignUp(
-        parentName: String,
-        parentProfileImage: String,
         navOptions: NavOptions? = null,
     ) = navController.navigateToParentSignUp(
-        parentName = parentName,
-        parentProfileImage = parentProfileImage,
         navOptions = navOptions
     )
 
