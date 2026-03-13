dcccb195ccafc111b6e6200138e600d8ae3e97fd
Kyu hyunSung <dmp100@naver.com>
2026-03-13 20:59:40 +0900

    feat/#103 : GlobalUIEventHolder에 tabReselectedEvent, onTabReselected 추가

--- app/src/main/java/com/kiero/core/model/trigger/GlobalUiEventHolder.kt
+++ app/src/main/java/com/kiero/core/model/trigger/GlobalUiEventHolder.kt
@@ -1,10 +1,14 @@
 package com.kiero.core.model.trigger
 
 import androidx.compose.runtime.Stable
+import com.kiero.core.navigation.Route
+import kotlinx.coroutines.flow.SharedFlow
 
 @Stable
 class GlobalUiEventHolder(
     val dialogTrigger: DialogTrigger,
     val showToast: (String) -> Unit,
-    val showSnackbar: (SnackbarState) -> Unit
+    val showSnackbar: (SnackbarState) -> Unit,
+    val tabReselectedEvent: SharedFlow<Route>,
+    val onTabReselected: (Route) -> Unit
 )
