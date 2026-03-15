b540b133e3b2d2bb35e18d67255a1629b3e82e2b
Kyu hyunSung <dmp100@naver.com>
2026-03-16 01:12:59 +0900

    refactor/#88 : AlarmService Method 변경

--- app/src/main/java/com/kiero/data/parent/alarm/remote/api/AlarmService.kt
+++ app/src/main/java/com/kiero/data/parent/alarm/remote/api/AlarmService.kt
@@ -2,12 +2,12 @@ package com.kiero.data.parent.alarm.remote.api
 
 import com.kiero.core.network.model.BaseResponse
 import com.kiero.data.parent.alarm.remote.dto.response.AlarmFeedResponseDto
-import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface AlarmService {
-    @GET("api/v1/feeds/{childId}")
+    @PATCH("api/v1/feeds/{childId}")
     suspend fun getAlarmFeed(
         @Path("childId") childId: Long,
         @Query("size") size: Int? = null,
