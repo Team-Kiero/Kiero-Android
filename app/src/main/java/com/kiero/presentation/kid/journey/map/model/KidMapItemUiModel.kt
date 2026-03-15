a52cb3315179d665ac6dce52b72352f5bc57f10d
seungjae708 <choesj365@gmail.com>
2026-03-15 13:48:06 +0900

    feat/#109: ScheduleProgressItemModel을 KidMapItemUiModel로 변환하는 확장 함수 구현

--- app/src/main/java/com/kiero/presentation/kid/journey/map/model/KidMapItemUiModel.kt
+++ app/src/main/java/com/kiero/presentation/kid/journey/map/model/KidMapItemUiModel.kt
@@ -1,6 +1,7 @@
 package com.kiero.presentation.kid.journey.map.model
 
 import androidx.compose.runtime.Immutable
+import com.kiero.data.kid.schedule.model.ScheduleProgressItemModel
 import com.kiero.presentation.kid.journey.model.KidJourneyStoneType
 
 @Immutable
@@ -11,4 +12,15 @@ data class KidMapItemUiModel(
     val isOngoing: Boolean = false,
     val stoneType: KidJourneyStoneType = KidJourneyStoneType.COURAGE,
     val status: KidMapScheduleStatus = KidMapScheduleStatus.PENDING
+)
+
+fun ScheduleProgressItemModel.toUiModel() = KidMapItemUiModel(
+    name = this.name,
+    startTime = this.startTime,
+    endTime = this.endTime,
+    isOngoing = this.isOngoing,
+    stoneType = runCatching { KidJourneyStoneType.valueOf(this.stoneType) }.getOrDefault(
+        KidJourneyStoneType.COURAGE
+    ),
+    status = KidMapScheduleStatus.from(this.status)
 )
