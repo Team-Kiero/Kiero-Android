36b59eb07a5da2e90bcc93b7cd7efd408fdc211a
sonms <smsms5676@naver.com>
2026-03-17 09:08:41 +0900

    mod/#110: 쿠폰 구매도 데이터 api 변경

--- app/src/main/java/com/kiero/data/kid/wish/remote/api/WishService.kt
+++ app/src/main/java/com/kiero/data/kid/wish/remote/api/WishService.kt
@@ -4,13 +4,14 @@ import com.kiero.core.network.model.BaseResponse
 import com.kiero.data.kid.wish.remote.dto.response.WishResponseDto
 import retrofit2.http.GET
 import retrofit2.http.PATCH
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface WishService {
     @GET("api/v1/coupons")
     suspend fun getCoupons() : BaseResponse<List<WishResponseDto>>
 
-    @PATCH("api/v1/coupons/{couponId}")
+    @POST("api/v1/coupons/{couponId}/purchase")
     suspend fun patchCoupon(
         @Path("couponId") couponId: Long,
     ) : BaseResponse<WishResponseDto>
