d7deb7a69bfb139b7629c30ded36a042ea42b3cb
sonms <smsms5676@naver.com>
2026-03-18 00:43:21 +0900

    mod/#110: 키어로 content empty 뷰 bottom height 추가

--- app/src/main/java/com/kiero/core/designsystem/component/emptyview/KieroContentEmptyScreen.kt
+++ app/src/main/java/com/kiero/core/designsystem/component/emptyview/KieroContentEmptyScreen.kt
@@ -26,7 +26,7 @@ fun KieroContentEmptyScreen(
     modifier: Modifier = Modifier,
     title: String? = "오늘 등록된 일정이 없어요.",
     description: String = "우측 하단 버튼을 눌러 일정을 추가해보세요!",
-    bottomHeight : Dp = 0.dp
+    bottomHeight : Dp = 50.dp
 ) {
     Box(
         modifier = modifier.fillMaxSize(),
@@ -34,7 +34,7 @@ fun KieroContentEmptyScreen(
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(4.dp)
+            verticalArrangement = Arrangement.Center
         ) {
             Icon(
                 imageVector = ImageVector.vectorResource(id = R.drawable.ic_parent_empty_state),
@@ -42,13 +42,13 @@ fun KieroContentEmptyScreen(
                 tint = Color.Unspecified
             )
 
-            Spacer(modifier = Modifier.height(7.dp))
+            Spacer(modifier = Modifier.height(11.dp))
 
             if (title != null) {
                 Text(
                     text = title,
-                    style = KieroTheme.typography.semiBold.title4,
-                    color = KieroTheme.colors.gray400
+                    style = KieroTheme.typography.semiBold.title3,
+                    color = KieroTheme.colors.gray500
                 )
             }
 
