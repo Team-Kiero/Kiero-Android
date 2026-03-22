03a8ba44857ae330ad90f98b9445628044716cdb
Son Juwan <wndhks171@naver.com>
2026-03-22 16:34:22 +0900

    [mod/#115]: 미션 메인 - 로딩뷰 수정

--- app/src/main/java/com/kiero/presentation/parent/screen/mission/ParentMissionScreen.kt
+++ app/src/main/java/com/kiero/presentation/parent/screen/mission/ParentMissionScreen.kt
@@ -46,6 +46,7 @@ import com.kiero.core.designsystem.component.chip.action.KieroCoinAction
 import com.kiero.core.designsystem.component.dialog.KieroDialog
 import com.kiero.core.designsystem.component.dialog.action.KieroCancelAction
 import com.kiero.core.designsystem.component.dialog.action.KieroConfirmAction
+import com.kiero.core.designsystem.component.indicator.KieroLoadingIndicator
 import com.kiero.core.designsystem.theme.KieroTheme
 import com.kiero.core.model.UiState
 import com.kiero.core.model.trigger.SnackbarState
@@ -108,10 +109,7 @@ fun ParentMissionRoute(
             navigateToAutoMission = navigateToAutoMission,
         )
 
-        is UiState.Loading -> Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center,
-        ) { CircularProgressIndicator(color = KieroTheme.colors.main) }
+        is UiState.Loading -> KieroLoadingIndicator()
 
         else -> Unit
     }
