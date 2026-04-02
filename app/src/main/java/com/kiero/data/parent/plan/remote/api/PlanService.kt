4a8b7c8773987e65abdb6c56c59a6d0692e633c8
Son Juwan <wndhks171@naver.com>
2026-04-03 00:01:15 +0900

    [mod/#122] 헤더 서버캐싱 삭제

--- app/src/main/java/com/kiero/data/parent/plan/remote/api/PlanService.kt
+++ app/src/main/java/com/kiero/data/parent/plan/remote/api/PlanService.kt
@@ -9,6 +9,7 @@ import com.kiero.data.parent.plan.remote.dto.response.PlanColorResponseDto
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.HTTP
+import retrofit2.http.Headers
 import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
@@ -26,6 +27,7 @@ interface PlanService {
         @Body planAddRequestDto: PlanAddRequestDto,
     ): BaseResponse<Unit>
 
+    @Headers("Cache-Control: no-cache")
     @GET("api/v1/schedules/{childId}")
     suspend fun getPlanAll(
         @Path("childId") childId: Long,
@@ -33,6 +35,7 @@ interface PlanService {
         @Query("endDate") endDate: String,
     ): BaseResponse<PlanAllResponseDto>
 
+    @Headers("Cache-Control: no-cache")
     @PATCH("api/v1/schedules/{scheduleId}")
     suspend fun updateSchedule(
         @Path("scheduleId") scheduleId: Long,
@@ -40,6 +43,7 @@ interface PlanService {
         @Body request: PlanUpdateRequestDto,
     ): BaseResponse<Unit>
 
+    @Headers("Cache-Control: no-cache")
     @HTTP(method = "DELETE", path = "api/v1/schedules/{scheduleId}", hasBody = true)
     suspend fun deleteSchedule(
         @Path("scheduleId") scheduleId: Long,
