d9813f4c5ce407f6513beb94bc0ef9eecdd64e8c
sonms <smsms5676@naver.com>
2026-04-22 10:52:14 +0900

    [mod#135] AuthScreen에 navigationBarsPadding 추가

--- app/src/main/java/com/kiero/presentation/auth/AuthScreen.kt
+++ app/src/main/java/com/kiero/presentation/auth/AuthScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -70,6 +71,7 @@ fun AuthScreen(
     Box(
         modifier = modifier
             .fillMaxSize()
+            .navigationBarsPadding()
     ) {
         Box(modifier = Modifier.fillMaxSize()) {
             Image(
