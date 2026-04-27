e74e8400ece5a267c54ac13fc9494374e833e7db
sonms <smsms5676@naver.com>
2026-04-27 13:51:24 +0900

    [mod#138] 일정 관련 라벨 조건 분기 수정

--- app/src/main/java/com/kiero/presentation/parent/screen/journey/model/TodayJourneyUiModel.kt
+++ app/src/main/java/com/kiero/presentation/parent/screen/journey/model/TodayJourneyUiModel.kt
@@ -2,6 +2,7 @@ package com.kiero.presentation.parent.screen.journey.model
 
 import com.kiero.core.common.extension.toShortTime
 import com.kiero.data.parent.journey.model.ParentJourneyScheduleModel
+import com.kiero.presentation.parent.screen.journey.extension.toLocalTime
 import com.kiero.presentation.parent.screen.journey.extension.toTodayStatus
 import java.time.LocalTime
 
@@ -37,8 +38,18 @@ fun ParentJourneyScheduleModel.toUiModel(
         isOngoing = isOngoing,
         todayStatus = todayStatus,
         scheduleLabel = when (todayStatus) {
-            TodayStatus.CURRENT_COMPLETED -> "현재 일정"
             TodayStatus.NEXT_UPCOMING -> "다음 일정"
+            TodayStatus.CURRENT_COMPLETED -> {
+                val start = startTime.toLocalTime()
+                when {
+                    // 조기 완료했지만 원래 '다음 일정' 순서였던 경우 -> "다음 일정" 유지
+                    isNextUpcoming && !hasOngoingSchedule -> "다음 일정"
+                    // 조기 완료했는데 '다음 일정'조차 아닌 아주 먼 미래의 일정인 경우 -> 라벨 숨김
+                    start != null && currentTime < start -> ""
+                    // 정상적으로 시작 시간이 지났거나 현재 진행 중인 일정인 경우 -> "현재 일정"
+                    else -> "현재 일정"
+                }
+            }
             else -> ""
         }
     )
