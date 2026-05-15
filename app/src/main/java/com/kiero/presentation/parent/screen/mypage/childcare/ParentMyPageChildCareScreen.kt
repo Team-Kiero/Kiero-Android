b8d21fa621db8a0e5abfd5fcb260146b73d9e5a5
sonms <smsms5676@naver.com>
2026-05-15 17:58:31 +0900

    [feat#161] 부모 마이페이지 공동 자녀 관리 관련 스크린 컴포넌트 생성
    - `ParentMyPageChildCareScreen` 컴포넌트 추가
    - `ParentMyPageChildManagementScreen` 컴포넌트 추가
    - `ParentMyPageChildInviteScreen` 컴포넌트 추가

---
+++ app/src/main/java/com/kiero/presentation/parent/screen/mypage/childcare/ParentMyPageChildCareScreen.kt
@@ -0,0 +1,20 @@
+package com.kiero.presentation.parent.screen.mypage.childcare
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.kiero.core.designsystem.theme.KieroTheme
+
+@Composable
+fun ParentMyPageChildCareScreen(
+
+) {
+
+}
+
+@Preview
+@Composable
+private fun ParentMyPageChildCareScreenPreview() {
+    KieroTheme {
+        ParentMyPageChildCareScreen()
+    }
+}
---
+++ app/src/main/java/com/kiero/presentation/parent/screen/mypage/childcare/route/ParentMyPageChildInviteScreen.kt
@@ -0,0 +1,21 @@
+package com.kiero.presentation.parent.screen.mypage.childcare.route
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.kiero.core.designsystem.theme.KieroTheme
+
+@Composable
+fun ParentMyPageChildInviteScreen(
+
+) {
+
+}
+
+@Preview
+@Composable
+private fun ParentMyPageChildInviteScreenPreview() {
+    KieroTheme {
+        ParentMyPageChildInviteScreen()
+
+    }
+}
