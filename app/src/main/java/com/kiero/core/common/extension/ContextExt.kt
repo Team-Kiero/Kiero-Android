6341d7efff221e4c2497943ca3133308d40ca70c
seungjae708 <choesj365@gmail.com>
2026-05-27 01:16:10 +0900

    refactor/#162: ContextExt.kt 제거

--- app/src/main/java/com/kiero/core/common/extension/ContextExt.kt
+++
@@ -1,9 +0,0 @@
-package com.kiero.core.common.extension
-
-import android.app.NotificationManager
-import android.content.Context
-
-fun Context.isNotificationEnabled(): Boolean {
-    val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-    return manager.areNotificationsEnabled()
-}
