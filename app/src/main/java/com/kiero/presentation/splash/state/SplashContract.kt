df6aef075d5395351294ddf2d2b21f449c0d6041
sonms <smsms5676@naver.com>
2026-05-28 20:48:29 +0900

    [mod#169] SplashViewModel: CheckParentAutoLoginUseCase 도입 및 부모 자동 로그인 로직 리팩터링

--- app/src/main/java/com/kiero/presentation/splash/state/SplashContract.kt
+++ app/src/main/java/com/kiero/presentation/splash/state/SplashContract.kt
@@ -2,8 +2,8 @@ package com.kiero.presentation.splash.state
 
 sealed interface SplashSideEffect {
     data object NavigateToAuth : SplashSideEffect
-    data object NavigateToParentHome : SplashSideEffect // 부모 스케쥴
-    data object NavigateToParentGraph : SplashSideEffect
+    data object NavigateToParentHome : SplashSideEffect // 부모 오늘의 현황
+    data object NavigateToParentGraph : SplashSideEffect // 카카오 로그인
     data object NavigateToKidHome : SplashSideEffect
     data object NavigateToKidOnboarding : SplashSideEffect
 }
--- app/src/main/java/com/kiero/presentation/splash/viewmodel/SplashViewModel.kt
+++ app/src/main/java/com/kiero/presentation/splash/viewmodel/SplashViewModel.kt
@@ -8,6 +8,8 @@ import com.kiero.core.localstorage.onboarding.OnboardingManager
 import com.kiero.core.model.auth.UserRole
 import com.kiero.core.network.auth.TokenRefreshService
 import com.kiero.data.auth.repository.AuthRepository
+import com.kiero.domain.parent.splash.model.ParentAutoLoginResult
+import com.kiero.domain.parent.splash.usecase.CheckParentAutoLoginUseCase
 import com.kiero.presentation.splash.state.SplashSideEffect
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
@@ -19,11 +21,10 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
-    private val authRepository: AuthRepository,
     private val tokenManager: TokenManager,
     private val onboardingManager: OnboardingManager,
-    private val userInfoManager: UserInfoManager,
     private val reIssueManager: TokenRefreshService,
+    private val checkParentAutoLoginUseCase: CheckParentAutoLoginUseCase
 ) : ViewModel() {
     private val _sideEffect = Channel<SplashSideEffect>()
     val sideEffect = _sideEffect.receiveAsFlow()
@@ -56,24 +57,21 @@ class SplashViewModel @Inject constructor(
     }
 
     private suspend fun handleParentLogin() {
-        authRepository.getChildren()
-            .onSuccess { children ->
-                Timber.e("children $children")
-                if (children.isEmpty()) {
-                    // 자녀가 없으면 카카오 로그인으로
-                    _sideEffect.send(SplashSideEffect.NavigateToParentGraph)
-                } else {
-                    // 자녀가 있으면 첫 번째 자녀 정보 저장 후 스케줄로
-                    Timber.e("children.first().childId ${children.first().childId}")
-                    userInfoManager.saveChildIdInfo(children.first().childId)
+        val result = checkParentAutoLoginUseCase()
 
-                    _sideEffect.send(SplashSideEffect.NavigateToParentHome)
-                }
-            }
-            .onFailure { t ->
-                Timber.e(t, "자녀 목록 조회 실패")
-                _sideEffect.send(SplashSideEffect.NavigateToAuth)
-            }
+        when (result) {
+            /*is ParentAutoLoginResult.MoveToTermsAgreement ->
+                _sideEffect.send(SplashSideEffect.NavigateToAuth)*/
+
+            is ParentAutoLoginResult.MoveToParentHome ->
+                _sideEffect.send(SplashSideEffect.NavigateToParentHome) // 자녀가 있으면 첫 번째 자녀 정보 저장 후 스케줄로 NavigateToParentHome
+
+            is ParentAutoLoginResult.MoveToOnboarding ->
+                _sideEffect.send(SplashSideEffect.NavigateToParentGraph) // 부모 카카오 로그인 화면
+
+            is ParentAutoLoginResult.MoveToAuth ->
+                _sideEffect.send(SplashSideEffect.NavigateToAuth) // 역할 선택 화면
+        }
     }
 
     // 아이 로그인 처리 분리
