ec94c15132884a9c8cbd6351ef500a7e1c488d47
sonms <smsms5676@naver.com>
2026-05-28 20:45:38 +0900

    [feat#169] 회원 탈퇴 UseCase 추가 및 토큰 삭제 로직 구현

---
+++ app/src/main/java/com/kiero/domain/parent/mypage/WithDrawUseCase.kt
@@ -0,0 +1,18 @@
+package com.kiero.domain.parent.mypage
+
+import com.kiero.core.localstorage.TokenManager
+import com.kiero.data.parent.mypage.parent.repository.ParentMyPageRepository
+import javax.inject.Inject
+
+class WithDrawUseCase @Inject constructor(
+    private val parentMyPageRepository: ParentMyPageRepository,
+    private val tokenManager: TokenManager
+) {
+    suspend operator fun invoke(): Result<Unit> {
+        val result = parentMyPageRepository.postWithdrawStatus()
+
+        return result.onSuccess {
+            tokenManager.clearTokens()
+        }
+    }
+}
