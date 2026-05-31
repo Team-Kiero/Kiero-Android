15d219aac2e3346cba9e11e9bf94805bd3a350de
seungjae708 <choesj365@gmail.com>
2026-05-31 16:24:13 +0900

    feat/#175: KidMySpace UI 모델 매핑 함수 추가

--- app/src/main/java/com/kiero/presentation/kid/myspace/state/KidMySpaceContract.kt
+++ app/src/main/java/com/kiero/presentation/kid/myspace/state/KidMySpaceContract.kt
@@ -1,6 +1,9 @@
 package com.kiero.presentation.kid.myspace.state
 
 import androidx.compose.runtime.Immutable
+import com.kiero.core.common.extension.toWishArchiveDateString
+import com.kiero.data.kid.wish.model.WishHistoryModel
+import com.kiero.presentation.kid.myspace.wisharchive.model.KidMySpaceWishArchiveItemUiModel
 
 @Immutable
 data class KidMySpaceState(
@@ -9,3 +12,12 @@ data class KidMySpaceState(
     val showNotificationDialog: Boolean = false,
     val permissionNotificationDeniedCount: Int = 0,
 )
+
+fun WishHistoryModel.toUiModel(index : Int) : KidMySpaceWishArchiveItemUiModel {
+    return KidMySpaceWishArchiveItemUiModel(
+        id = index.toLong(),
+        name = name,
+        acquiredAt = purchasedAt.toWishArchiveDateString(),
+        price = price
+    )
+}
