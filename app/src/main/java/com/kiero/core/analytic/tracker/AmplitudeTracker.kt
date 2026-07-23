d92028b9480eb2d058a19669748e69c8c80a2787
Son Juwan <wndhks171@naver.com>
2026-07-23 23:39:06 +0900

    [feat/#197] 모듈 및 의존성 주입

---
+++ app/src/main/java/com/kiero/core/analytic/tracker/AmplitudeTracker.kt
@@ -0,0 +1,24 @@
+package com.kiero.core.analytic.tracker
+
+import com.amplitude.android.Amplitude
+import com.amplitude.android.events.Identify
+import com.kiero.core.analytic.type.AnalyticsEvent
+import com.kiero.core.analytic.type.UserProperty
+import javax.inject.Inject
+
+class AmplitudeTracker @Inject constructor(
+    private val amplitude: Amplitude,
+) : Tracker {
+
+    override fun track(event: AnalyticsEvent, properties: Map<String, Any?>) {
+        amplitude.track(
+            eventType = event.eventName,
+            eventProperties = properties.filterValues { it != null } as Map<String, Any>,
+        )
+    }
+
+    override fun setUserProperty(property: UserProperty) {
+        val identify = Identify().set(property.key, property.value)
+        amplitude.identify(identify)
+    }
+}
